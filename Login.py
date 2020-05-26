f07fc32a97fe9bd2ab8a8c030fb317ec839b51cd
jq3219 <jq3210@naver.com>
2020-05-26 21:11:13 +0900

    game init view

--- Login.py
+++ Login.py
@@ -1,6 +1,18 @@
 import pygame
+import sys
+from pygame.color import THECOLORS
 
-class Login_Main_View:
-    def __init__(self,id,pw):
-        id = self.id
-        pw = self.pw
+pygame.init()
+
+screen = pygame.display.set_mode([640,480])                 #set_mode([width,hegith])
+screen.fill([255,255,255])                                  #fill - 색 채우기
+pygame.draw.circle(screen,THECOLORS["blue"],[320,240],30) #pygame.draw.circle(surface, color, center, radius)
+
+pygame.display.flip()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
