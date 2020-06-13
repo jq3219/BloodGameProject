1859aa997f7b34d0103e9cf23d0730f81664a6f7
jq3219 <jq3210@naver.com>
2020-06-13 15:39:19 +0900

    login check and signup

--- Login.py
+++ Login.py
@@ -1,7 +1,8 @@
+
 import pygame, pygame.font
 import sys
 import tmpdb
-import tmpexample
+# import tmpexample
 from pygame.color import THECOLORS
 
 WHITE   = (255,255,255)
@@ -10,50 +11,52 @@ RED     = (255,0,0)
 GREEN   = (0,255,0)
 BLUE    = (0,0,255)
 
+pygame.init()
 #pygame 모듈 이니시
 screen = pygame.display.set_mode([620,480])  #set_mode([width,hegith])
 pygame.display.set_caption("Game Title")
 
-#로그인 화면 기본 설정
-def LoginScreen():
-    #id 또는 pw 가 일치할 경우 login 성공일 떄 창 닫기
-    id_success = False;
-    pw_success = False;
-    #프로그램 종료(pygame.quit()) 종료일 떄 창 닫기
-    done = False;
+#폰트 초기화
+font = pygame.font.SysFont("consolas",15,20,3)
+pygame.font.init()
 
-    id_active  = False
-    pw_active  = False
-    id_text = 'ID : '
-    id_input = ''
-    pw_text = 'PW : '
-    pw_input = ''
-    clock   = pygame.time.Clock()
+class Login:
+    def __init__(self):
+        #id 또는 pw 가 일치할 경우 login 성공일 떄 창 닫기
+        self.id_success = False;
+        self.pw_success = False;
+        #프로그램 종료(pygame.quit()) 종료일 떄 창 닫기
+        self.done = False;
 
-    action_flag = False
+        #rect활성화 변수
+        self.id_active  = False
+        self.pw_active  = False
 
-    #폰트 초기화
-    font = pygame.font.SysFont("consolas",15,20,3)
-    pygame.font.init()
+        #입력값 받는 변수
+        self.id_input = ''
+        self.pw_input = ''
 
-    #로그인 처리
-    #id와 pw가 모두 true값으로 고쳐지면 프로그램 나오기
-    #혹은 done 이 true로 고쳐지면 프로그램 나오기
-    while (not (id_success and pw_success)) and not done:
+        #user id,pw 입력 받을 시1로 변경 -> user 정보 입력 체크
+        self.user_check = [0,0]
+        self.clock      = pygame.time.Clock()
+
+    def Login_Setting(self):
+        global screen
         screen.fill(WHITE)
-
         #pygame.draw.rect(surface,color,rect,width(==border))
         #rect(left,top,width,height) -> type
+
         #id
-        id_border       = pygame.draw.rect(screen,BLACK,(screen.get_width() - 400,screen.get_height() - 200,200,20),1)
-        id_text_rect    = pygame.draw.rect(screen,WHITE,(id_border[0]+1,id_border[1]+1,29,18),0)
-        id_input_rect   = pygame.draw.rect(screen,WHITE,(id_text_rect[0]+45,id_text_rect[1],124,18),0)
-
+        id_border           = pygame.draw.rect(screen,BLACK,(screen.get_width() - 400,screen.get_height() - 200,200,20),1)
+        id_text_rect        = pygame.draw.rect(screen,WHITE,(id_border[0]+1,id_border[1]+1,29,18),0)
+        self.id_input_rect  = pygame.draw.rect(screen,WHITE,(id_text_rect[0]+45,id_text_rect[1],124,18),0)
         #pw
-        pw_border       = pygame.draw.rect(screen,BLACK,(screen.get_width() - 400,screen.get_height() - 180,200,20),1)
-        pw_text_rect    = pygame.draw.rect(screen,WHITE,(pw_border[0]+1,pw_border[1]+1,29,18),0)
-        pw_input_rect   = pygame.draw.rect(screen,WHITE,(pw_text_rect[0]+45,pw_text_rect[1],124,18),0)
+        pw_border           = pygame.draw.rect(screen,BLACK,(screen.get_width() - 400,screen.get_height() - 180,200,20),1)
+        pw_text_rect        = pygame.draw.rect(screen,WHITE,(pw_border[0]+1,pw_border[1]+1,29,18),0)
+        self.pw_input_rect  = pygame.draw.rect(screen,WHITE,(pw_text_rect[0]+45,pw_text_rect[1],124,18),0)
 
+        id_text = 'ID : '
+        pw_text = 'PW : '
 
         #고정 text surface 랜더링
         id_text_surface = font.render(id_text,True,BLACK)
@@ -64,69 +67,86 @@ def LoginScreen():
         screen.blit(id_text_surface,(id_text_rect.x+5,id_text_rect.y+2.5))
         screen.blit(pw_text_surface,(pw_text_rect.x+5,pw_text_rect.y+2.5))
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                    done = True
-            #마우스가 클릭이 특정부분에 눌리면 active 활성화 (이때 active는 플래그와 같은 역할 )
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                #기본은 id , pw active 비활성화
-                id_active = False
-                pw_active = False
-                #클릭 됐을 경우 active 활성화
-                if id_input_rect.collidepoint(event.pos):
-                    id_active = not id_active
-                elif pw_input_rect.collidepoint(event.pos):
-                    pw_active = not pw_active
 
-            if event.type == pygame.KEYDOWN:
-                if id_active:
+        #-----------------------------------------------------------------#
+        #회원가입
+        sign_up_border      = pygame.draw.rect(screen,BLACK,(screen.get_width() - 275,screen.get_height() - 155,75,20),1)
+        self.sign_up_rect   = pygame.draw.rect(screen,WHITE,(sign_up_border[0]+1,sign_up_border[1]+1,73,18))
+        sign_up_text        = "Sign Up"
+        sign_up_surface     = font.render(sign_up_text,True,BLACK)
+        screen.blit(sign_up_surface,(self.sign_up_rect.x+2.5,self.sign_up_rect.y+2.5))
+
+
+    #로그인 화면 기본 설정
+    def LoginScreen(self):
+        #로그인 처리
+        #id와 pw가 모두 true값으로 고쳐지면 프로그램 나오기
+        #혹은 done 이 true로 고쳐지면 프로그램 나오기
+        while (not (self.id_success and self.pw_success)) and not self.done:
+            self.Login_Setting()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.done = True
+                #마우스가 클릭이 특정부분에 눌리면 active 활성화 (이때 active는 플래그와 같은 역할 )
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    #기본은 id , pw active 비활성화
+                    self.id_active = False
+                    self.pw_active = False
+                    #클릭 됐을 경우 active 활성화
+                    if self.id_input_rect.collidepoint(event.pos):
+                        self.id_active = not self.id_active
+                    elif self.pw_input_rect.collidepoint(event.pos):
+                        self.pw_active = not self.pw_active
+                    elif self.sign_up_rect.collidepoint(event.pos):
+                        sys_id = int(tmpdb.select_max_id_num()) + 1
+                        tmpdb.sign_up(sys_id,self.id_input.strip(),self.pw_input.strip())
+
+                if event.type == pygame.KEYDOWN:
+                    if self.id_active:
+                        if event.key == pygame.K_BACKSPACE:
+                            self.id_input = self.id_input[:-1]
+                        else:
+                            self.id_input += event.unicode
+                    elif self.pw_active:
+                        if event.key == pygame.K_BACKSPACE:
+                            self.pw_input = self.pw_input[:-1]
+                        else:
+                            self.pw_input += event.unicode
+
                     if event.key == pygame.K_RETURN:
-                        id_success = True
-                        id_text_rect = 'ID : '
-                        print(id_input)
-                    elif event.key == pygame.K_BACKSPACE:
-                        id_input = id_input[:-1]
+                        #strip 문자 공백 제거 함수 (안써주면 db들어갈 때 2번 째 인자에서 오류 생김)
+                        self.user_check[0] = self.id_input.strip()
+                        self.user_check[1] = self.pw_input.strip()
+
+                        print("id : ",self.user_check[0])
+                        print("pw : ",self.user_check[1])
+
+                # 로그인에 성공할 경우 user_info를 리턴
+                if not self.user_check[0]==0 and not self.user_check[1]==0:
+                    if tmpdb.login_check(self.user_check[0],self.user_check[1]) == True:
+                        print("login success")
+                        return self.user_check
                     else:
-                        id_input += event.unicode
-                elif pw_active:
-                    if event.key == pygame.K_RETURN:
-                        pw_success = True
-                        pw_text_rect = 'PW : '
-                        print(pw_input)
-                    elif event.key == pygame.K_BACKSPACE:
-                        pw_input = pw_input[:-1]
-                    else:
-                        pw_input += event.unicode
+                        self.user_check[0] = 0
+                        self.user_check[1] = 0
+                        print("login false")
+                        continue
 
+            #사용자에게 받아온 입력 값 랜더링
+            id_input_surface = font.render(self.id_input,True,BLACK)
+            pw_input_surface = font.render(self.pw_input,True,BLACK)
 
+            #사용자에게 받아온 입력 값 화면에 붙여주기
+            screen.blit(id_input_surface,(self.id_input_rect.x+5,self.id_input_rect.y+2.5))
+            screen.blit(pw_input_surface,(self.pw_input_rect.x+5,self.pw_input_rect.y+2.5))
 
-            # 로그인에 성공할 경우 user_info를 리턴
-            if tmpdb.login_check(id_input,pw_input) == True:
-                user_info = []
-                user_info.append([id_input,pw_input])
-                print("login success")
-                return user_info
-            else:
-                print("login false")
-                continue
-
-        #사용자에게 받아온 입력 값 랜더링
-        id_input_surface = font.render(id_input,True,BLACK)
-        pw_input_surface = font.render(pw_input,True,BLACK)
-
-        #사용자에게 받아온 입력 값 화면에 붙여주기
-        screen.blit(id_input_surface,(id_input_rect.x+5,id_input_rect.y+2.5))
-        screen.blit(pw_input_surface,(pw_input_rect.x+5,pw_input_rect.y+2.5))
-
-        #display 새로고침
-        pygame.display.flip()
+            #display 새로고침
+            pygame.display.flip()
 
 def main():
-    pygame.init()
-
-    if LoginScreen() == True:
-        tmpexample.game(screen,user_info)
-
-
+    lg = Login()
+    lg.LoginScreen()
+    # if LoginScreen() == True:
+        # tmpexample.game(screen,user_info)
 
 if __name__ == '__main__':  main()
--- tmpdb.py
+++ tmpdb.py
@@ -1,36 +1,43 @@
 import pymysql
 
-
 #with as 구문
 #with (파일 경로, 모드) as 파일 객체
 #파일을 열고 with as 구문이 끝날때 자동적으로 파일을 닫아준다 (close가 따로 필요없음)
 
 conn = pymysql.connect(user='root',passwd='power900!', db='test2',charset='utf8')
 
-def insert_sql(params):
+def sign_up(sys_id,user_id,user_pw):
     try:
         with conn.cursor() as curs:
-            print(params)
-            insert_sql = "insert into t_user values(%s,%s,%s)"
-            curs.execute(insert_sql,params[0])
-            conn.commit()
-    finally:
-        pass
-        # conn.close()
+            signUp_sql = "insert into t_user values(%s,%s,%s)"
+            if curs.execute(signUp_sql,(sys_id,user_id,user_pw)):
+                print('Sign up Success')
+                conn.commit()
+            else:
+                print('Sign up failed')
 
-def login_check(id_input,pw_input):
+    except Exception as e:
+        print(e)
+
+def login_check(user_id,user_pw):
     try:
         with conn.cursor() as curs:
-            loginChk_sql = "select user_id,user_pw from t_user where user_id = %s and user_pw = %s"
-            curs.execute(loginChk_sql,(id_input,pw_input))
+            user_check = [0,0]
+            loginChk_sql = "select user_id,user_pw from t_user where user_id = %s or user_pw = %s"
+            curs.execute(loginChk_sql,(user_id,user_pw))
+            res = curs.fetchone()
+            #res가 None일 경우 상정하지 않으면 값을 못읽었을 경우 오류
+            if not res == None and user_id == res[0]:
+                user_check[0] = 1
+            if not res == None and user_pw == res[1]:
+                user_check[1] = 1
 
-            if curs.fetchone() != None:
+            if user_check[0] and user_check[1]:
                 return True
             else:
                 return False
-    finally:
-        pass
-    #     conn.close()
+    except Exception as e:
+        print(e)
 
 def select_max_id_num():
     try:
@@ -39,13 +46,13 @@ def select_max_id_num():
             curs.execute(maxIdSql)
             maxIdNum = curs.fetchone()
             return maxIdNum[0]
-    finally:
-        pass
-    #     conn.close()
-# 
+    except Exception as e:
+        print(e)
+#
 # def main():
-#     id_input = 'test1'
-#     pw_input = 'test1'
-#     login_check(id_input,pw_input)
+    # print('select_max_id_num : ', select_max_id_num())
+#     user_id = 'test1'
+#     user_pw = 'test1'
+#     login_check(user_id,user_pw)
 #
 # if __name__=='__main__': main()
